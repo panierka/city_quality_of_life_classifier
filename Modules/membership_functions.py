e651327c110ff0be3b657e7c267087bbe13ba135
jdylik <jan.karol.dylik@gmail.com>
2022-06-04 13:00:57 +0200

    Pierwszy

--- Modules/membership_functions.py
+++ Modules/membership_functions.py
@@ -27,3 +27,27 @@ class TriangularFunction(Function):
 
         if x > self.__c:
             return 0
+
+
+class TrapezoidalFunction(Function):
+    def __init__(self, a: float, b: float, c: float, d: float):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+        self.__d = d
+
+    def calculate(self, x: float) -> float:
+        if x <= self.__a:
+            return 0
+
+        if self.__a < x <= self.__b:
+            return (x - self.__a) / (self.__b - self.__a)
+
+        if self.__b < x <= self.__c:
+            return 1
+
+        if self.__c < x <= self.__d:
+            return (self.__d - x) / (self.__d - self.__c)
+
+        if x > self.__c:
+            return 0
