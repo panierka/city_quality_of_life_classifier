371854582cf95f3f0231a9d3803b4dd20ce92894
jdylik <jan.karol.dylik@gmail.com>
2022-06-07 22:41:56 +0200

    Membership functions

--- Modules/membership_functions.py
+++ Modules/membership_functions.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta, abstractmethod
-
+import math
 
 class Function:
     __metaclass__ = ABCMeta
@@ -54,3 +54,46 @@ class TrapezoidalFunction(Function):
 
         if x > self.__c:
             return 0
+
+class GaussianFunction(Function):
+    def __init__(self, stdev: float, mean: float):
+        self.__stdev = stdev
+        self.__mean = mean
+
+    def calculate(self, x: float) -> float:
+        return 1 / ((2 * math.pi) ** (1 / 2) * stdev) * math.e ** (-((x - mean) ** 2) / (2 * (stdev ** 2)))
+
+class GammaFunction(Function):
+    def __init__(self, a: float, b: float):
+        self.__a = a
+        self.__b = b
+
+    def calculate(self, x: float) -> float:
+        if x <= self.__a:
+            return 0
+
+        if self.__a < x <= self.__b:
+            return (x - self.__a) / (self.__b - self.__a)
+
+        if x > self.__b:
+            return 1
+
+class BellFunction(Function):
+    def __init__(self, a: float, b: float, c: float):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+
+    def calculate(self, x: float) -> float:
+        if x <= self.__a:
+            return 0
+
+        if self.__a < x <= self.__b:
+            return 2*(((x - self.__a) / (self.__c - self.__a))**2)
+
+
+        if self.__b < x <= self.__c:
+            return 1-2*(((x - self.__a) / (self.__c - self.__a))**2)
+
+        if x > self.__c:
+            return 1
